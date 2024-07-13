daadda65e3686331dc64b16af5c075b7bb7db56e
remdeveloper15 <helamanfranco@hotmail.com>
2024-07-13 00:20:30 -0500

    Week 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square _shape = new Square("Red ", 2);
+        //Console.Write("Color: " + _shape.GetColor());
+        //Console.WriteLine("Area: " + _shape.GetArea());
+
+        Rectangle _shape1 = new Rectangle("Blue ", 2, 2);
+        //Console.Write("Color: " + _shape1.GetColor());
+        //Console.WriteLine("Area: " + _shape1.GetArea());
+
+        Circle _shape2 = new Circle("Yellow ", 2);
+        //Console.Write("Color: " + _shape2.GetColor());
+        //Console.WriteLine("Area: " + _shape2.GetArea());
+
+        List<Shape> _shapes = new List<Shape>();
+        _shapes.Add(_shape);
+        _shapes.Add(_shape1);
+        _shapes.Add(_shape2);
+
+        foreach (Shape s in _shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
+
     }
 }
---
+++ prove/Develop05/GoalManager.cs
@@ -0,0 +1,16 @@
+public class GoalManager
+{
+    private List<string> _goals = new List<string>();
+    private int _score;
+
+    public GoalManager()
+    {
+
+    }
+
+    public void Start()
+    {
+
+    }
+
+}
